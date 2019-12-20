eaa69231bd161edf49d5ef65a8087279c135983a
Kévin Joya <kevin.joya@outlook.com>
2019-12-20 13:56:25 +0100

    Added ref and dispatch on input value

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,17 +2,24 @@ import React, {useRef} from "react";
 import store from "../redux/store";
 
 function Buttons() {
-  const inputRef = useRef(null);
+  const inputRefAdd = useRef(null);
+  const inputRefRemove = useRef(null);
 
   return(
     <React.Fragment>
       <div className="counter--buttons">
         <button className="btn btn-primary" onClick= {() => store.dispatch({type: "INCREMENT"})}>+</button>
-        <button className="btn btn-secondary" onClick= {() => store.dispatch({type: "DECREMENTE"})}>-</button>
+        <button className="btn btn-danger" onClick= {() => store.dispatch({type: "DECREMENTE"})}>-</button>
       </div>
       <div className="counter--input mt-5">
-        <button className="btn btn-primary mr-4" onClick={() => store.dispatch({type: "INCREMENT_WITH_DATA", value: inputRef }) }>Add</button>
-        <input type="number" id="counter--input--number" ref={inputRef}></input>
+        <div className="counter--input--increment mb-5">
+          <button className="btn btn-primary mr-4" onClick={() => store.dispatch({type: "INCREMENT_WITH_DATA", value: inputRefAdd }) }>Add</button>
+          <input type="number" id="counter--input--number" ref={inputRefAdd}></input>
+        </div>
+        <div className="counter--input--decremente ml-4">
+          <button className="btn btn-danger mr-4" onClick={() => store.dispatch({type: "DECREMENTE_WITH_DATA", value: inputRefRemove }) }>Remove</button>
+          <input type="number" id="counter--input--number" ref={inputRefRemove}></input>
+        </div>
       </div>
     </React.Fragment>
   )
